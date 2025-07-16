aab6a898aa134d7c7220edb8be33b4f7ec514afa
logan1x <sharmakhushal78@gmail.com>
2025-07-16 17:10:21 +0530

    removed comments

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,3 @@
-// import { Pool } from "pg";
-// import { drizzle } from "drizzle-orm/node-postgres";
-// import { loadEnvConfig } from "@next/env";
-
-// // Load Next.js environment variables
-// const projectDir = process.cwd();
-// loadEnvConfig(projectDir);
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
-
-// export const db = drizzle(pool);
-
 import { drizzle } from "drizzle-orm/neon-http";
 import { schema } from "./schema";
 

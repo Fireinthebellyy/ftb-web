482cd8e2debb31a5f219276c3bfffe68625cfe65
Amritesh Singh <amriteshsingh0006@gmail.com>
2025-09-07 04:10:04 -0700

    extra space removed

--- components/profile/OpportunityInterestSelector.tsx
+++ components/profile/OpportunityInterestSelector.tsx
@@ -13,7 +13,6 @@ import { Input } from "@/components/ui/input";
 import { Control, useWatch } from "react-hook-form";
 import { cn } from "@/lib/utils";
 
-
 type Props = {
   control: Control<any>;
   isEditing: boolean;

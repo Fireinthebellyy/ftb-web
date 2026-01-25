005aeb39b74403e7cbacf9fed9b2aecd27ac4f73
Khushal Sharma <sharmakhushal78@gmail.com>
2026-01-25 19:04:36 +0530

    K/opportunity image bug (#159)
    * image upload errro fixed with better error message
    
    * added test for new opportunity creation

---
+++ vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
+    css: false,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
+  define: {
+    "process.env": process.env,
+  },
+});

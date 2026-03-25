eb9bb16708164635e34804f66698b3806bc76644
Pranavi <pranavijawalekar2005@gmail.com>
2026-03-25 10:30:35 +0530

    dashbaord changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import typescriptEslint from "typescript-eslint";
 
 const eslintConfig = [

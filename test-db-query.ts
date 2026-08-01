fef5b25a2cdc108b61aa001ffa1ba62c0ba2b703
shivohams27-dev <shourayasharma27@gmail.com>
2026-08-02 00:54:12 +0530

    buddy-v0.8

--- test-db-query.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./lib/db";
-import { popups } from "./lib/schema";
-
-async function main() {
-  const allPopups = await db.select().from(popups);
-  console.log(allPopups);
-}
-main();

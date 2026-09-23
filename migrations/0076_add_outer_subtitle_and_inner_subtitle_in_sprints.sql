11acc62ce20e331f10e799841c879e97e77bb012
maanu123hyok <hyokaggarwal@gmail.com>
2026-09-24 00:47:50 +0530

    added inner and outer subtitle in post method

--- migrations/0076_add_outer_subtitle_and_inner_subtitle_in_sprints.sql
+++ migrations/0076_add_outer_subtitle_and_inner_subtitle_in_sprints.sql
@@ -1,5 +1,8 @@
 ALTER TABLE sprints
-    ADD COLUMN IF NOT EXISTS outer_subtitle TEXT;
+    ADD COLUMN IF NOT EXISTS outer_subtitle TEXT,
+    ADD COLUMN IF NOT EXISTS inner_subtitle TEXT;
 
-ALTER TABLE sprints
-    RENAME COLUMN subtitle TO inner_subtitle;
+UPDATE sprints
+SET inner_subtitle = subtitle
+WHERE inner_subtitle IS NULL
+  AND subtitle IS NOT NULL;
